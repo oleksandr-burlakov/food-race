26e9e60dfa0090da71d2d1abdb35a06604781a47
Oleksandr O.B. Burlakov <oleksandr.burlakov@devcom.com>
2026-08-01 23:04:47 +0300

    Migrated to angular 22

--- frontend/src/app/pages/auth/auth.component.ts
+++ frontend/src/app/pages/auth/auth.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-auth',
   imports: [],
   templateUrl: './auth.component.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './auth.component.scss',
 })
 export class AuthComponent {}
