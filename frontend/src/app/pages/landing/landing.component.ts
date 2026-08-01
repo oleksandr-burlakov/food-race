182572b01529c17e6d3c203ca3a6acffdd3a920c
Oleksandr Burlakov <83636875+oleksandr-burlakov@users.noreply.github.com>
2026-08-01 22:06:18 +0200

    Frontend migrations (#6)
    * Migrated angular to 21
    
    * installed latest packages
    
    * Migrated to angular 22
    
    ---------
    
    Co-authored-by: Oleksandr O.B. Burlakov <oleksandr.burlakov@devcom.com>

--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   imports: [],
   templateUrl: './landing.component.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './landing.component.scss',
 })
 export class LandingComponent {}
